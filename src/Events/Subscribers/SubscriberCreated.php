dc64f011059933693bd36ca10623a01b48396caa
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-06-05 09:31:38 +0200

    Add subscriber created event

---
+++ src/Events/Subscribers/SubscriberCreated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Eduka\Cube\Events\Domains;
+
+use Eduka\Cube\Models\Course;
+use Eduka\Cube\Models\Subscriber;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * This event is triggered each time a new domain is saved into the database.
+ */
+class SubscriberCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Subscriber $subscriber;
+    public Course $course;
+
+    public function __construct(Subscriber $subscriber, Course $course)
+    {
+        $this->subscriber = $subscriber;
+        $this->course = $course;
+    }
+}
