5d8881957acf08e5dc61f572856c32e912f7f2a8
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-03 20:32:22 +0100

    wip

--- src/Observers/OrderObserver.php
+++ src/Observers/OrderObserver.php
@@ -2,14 +2,19 @@
 
 namespace Eduka\Cube\Observers;
 
+use Eduka\Cube\Events\Orders\OrderCreatedEvent;
 use Eduka\Cube\Models\Order;
-use Eduka\Services\OrderCreatedEvent;
+use Eduka\Cube\Models\Variant;
 
 class OrderObserver
 {
     public function saving(Order $order)
     {
         $order->validate();
+
+        if ($order->variant_id) {
+            $order->course_id = Variant::find($order->variant_id)->course_id;
+        }
     }
 
     public function created(Order $order)
